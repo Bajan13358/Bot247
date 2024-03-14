1ca60b1719b975a71308a42edded5b86e807b838
Bajan13358 <111724128+Bajan13358@users.noreply.github.com>
2024-03-14 04:53:55 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
+let currentBot = null;
+
 const express = require("express");
 const http = require("http");
 const mineflayer = require('mineflayer')
 const pvp = require('mineflayer-pvp').plugin
-const { pathfinder, Movements, goals} = require('mineflayer-pathfinder')
 const armorManager = require('mineflayer-armor-manager')
 const AutoAuth = require('mineflayer-auto-auth');
 const app = express();
 
 app.use(express.json());
 
-// Página HTML para exibir informações do bot
 app.get("/", (req, res) => {
   let html = `
     <!DOCTYPE html>
@@ -17,17 +17,59 @@ app.get("/", (req, res) => {
     <head>
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Bot Status</title>
+      <title>Status do Bot</title>
       <style>
         body {
           font-family: Arial, sans-serif;
           text-align: center;
+          background-color: #f0f0f0;
+          padding: 20px;
+        }
+        h1 {
+          color: #333;
+        }
+        .menu {
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          margin-top: 20px;
+          background-color: #fff;
+          padding: 20px;
+          border-radius: 10px;
+          box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        }
+        #botInfo {
+          margin-bottom: 20px;
+        }
+        footer {
+          margin-top: 50px;
+          font-size: 12px;
+          color: #888;
+        }
+        .social-links {
+          margin-top: 10px;
+        }
+        .social-links a {
+          margin: 0 5px;
         }
       </style>
     </head>
     <body>
       <h1>Status do Bot</h1>
       <div id="botInfo"></div>
+      <div class="menu">
+        <div id="health">Vida: </div><br>
+        <div id="hunger">Fome: </div><br>
+        <div id="position">Posição: </div>
+      </div>
+      <footer>
+        © 2024 DuduSync - Criado por DuduSync
+        <div class="social-links">
+          <a href="https://4br.me/TelegramDuduSync" target="_blank">Telegram</a>
+          <a href="https://4br.me/WhatsDuduSync" target="_blank">WhatsApp</a>
+          <a href="https://4br.me/InstagramDuduSync" target="_blank">Instagram</a>
+        </div>
+      </footer>
       <script>
         function updateBotInfo() {
           fetch('/botInfo')
@@ -35,14 +77,25 @@ app.get("/", (req, res) => {
             .then(data => {
               const botInfoElement = document.getElementById('botInfo');
               botInfoElement.innerHTML = \`<p>Nome do Bot: \${data.username}</p>
-                                          <p>Tempo Online: \${data.onlineTime}</p>
-                                          <p>Vida: \${data.health}/20</p>
-                                          <p>Fome: \${data.food}/20</p>
-                                          <p>Cordenadas: X: \${data.position.x.toFixed(0)}, Y: \${data.position.y.toFixed(0)}, Z: \${data.position.z.toFixed(0)}</p>\`;
+                                          <p>Tempo Online: \${data.onlineTime}</p>\`;
+
+              const healthElement = document.getElementById('health');
+              healthElement.textContent = \`Vida: \${data.health}/20\`;
+
+              const hungerElement = document.getElementById('hunger');
+              hungerElement.textContent = \`Fome: \${data.hunger}/20\`;
+
+              const positionElement = document.getElementById('position');
+              positionElement.textContent = \`Posição: \${data.position ? Math.round(data.position.x) + ', ' + Math.round(data.position.y) + ', ' + Math.round(data.position.z) : 'Desconhecida'}\`;
             });
         }
-        setInterval(updateBotInfo, 1000); // Atualizar a cada segundo
-        updateBotInfo(); // Atualizar imediatamente ao carregar a página
+        setInterval(updateBotInfo, 1000);
+        updateBotInfo(); 
+
+        function clearConsole() {
+          console.clear();
+        }
+        setInterval(clearConsole, 1000);
       </script>
     </body>
     </html>
@@ -50,19 +103,18 @@ app.get("/", (req, res) => {
   res.send(html);
 });
 
-// Rota para fornecer informações do bot em formato JSON
 app.get("/botInfo", (req, res) => {
   if (currentBot) {
     const botInfo = {
       username: currentBot.username,
       onlineTime: calculateOnlineTime(currentBot.startTime),
-      health: Math.round(currentBot.health), // Arredonda para o número inteiro mais próximo
-      food: currentBot.food,
-      position: currentBot.entity.position
+      hunger: Math.floor(currentBot.food),
+      health: Math.floor(currentBot.health),
+      position: currentBot.entity ? currentBot.entity.position : null
     };
     res.json(botInfo);
   } else {
-    res.json({ username: "Nenhum bot está online", onlineTime: "", health: 0, food: 0, position: { x: 0, y: 0, z: 0 } });
+    res.json({ username: "Nenhum bot está online", onlineTime: "", hunger: 0, health: 0, position: null });
   }
 });
 
@@ -74,73 +126,53 @@ setInterval(() => {
   http.get(`http://${process.env.PROJECT_DOMAIN}.repl.co/`);
 }, 224000);
 
-let currentBot = null;
-
 function createBot() {
   if (currentBot) {
     currentBot.quit();
   }
 
   const randomUsername = 'Z' + Math.floor(Math.random() * 10000);
-  
+
   currentBot = mineflayer.createBot({
-    host: 'seriedb.aternos.me',
-    version: false,
-    username: randomUsername,
-    port: 31773,
-    gamemode: 3, // Modo de jogo espectador
-    plugins: [AutoAuth],
-    AutoAuth: 'bot112022'
+    host: 'seriedb.aternos.me', // Endereço do servidor Minecraft
+    version: false, // Não mexer
+    username: randomUsername, // Não mexer
+    port: 31773, // Porta do servidor Minecraft
+    plugins: [AutoAuth], // Não mexer
+    AutoAuth: 'bot112022' // Não mexer
   });
 
   currentBot.loadPlugin(pvp);
   currentBot.loadPlugin(armorManager);
-  currentBot.loadPlugin(pathfinder);
 
-  currentBot.startTime = new Date(); // Definindo a propriedade startTime
+  currentBot.startTime = new Date();
+
+  currentBot.on('login', () => {
+    console.log("Bot: " + currentBot.username + " entrou no servidor.");
+  });
 
   currentBot.on('kicked', (reason, loggedIn) => {
     if (loggedIn) {
+      console.log("O bot foi expulso do servidor.");
       setTimeout(() => {
+        console.log("Gerando outro bot...");
         createBot();
-      }, 5000);
+      }, 1000);
     }
   });
 
-  currentBot.on('chat', (username, message) => {
-    if (message === 'vem') {
-      const player = currentBot.players[username];
-      if (!player || !player.entity) {
-        currentBot.chat("Não posso encontrar você, " + username + "!");
-        return;
-      }
-      const { x, y, z } = player.entity.position;
-      const mcData = require('minecraft-data')(currentBot.version);
-      const movements = new Movements(currentBot, mcData);
-      currentBot.pathfinder.setMovements(movements);
-
-      currentBot.chat(`Estou indo, ${username}!`);
-      
-      currentBot.pathfinder.setGoal(new goals.GoalNear(x, y, z, 1)); // Mover-se para uma posição próxima do jogador
-    }
-  });
-
-  currentBot.on('kicked', console.log);
-  currentBot.on('error', console.log);
+  currentBot.on('error', console.error);
 }
 
 function calculateOnlineTime(startTime) {
   if (!startTime) return "";
-
   const now = new Date();
   const diff = now - startTime;
   const seconds = Math.floor(diff / 1000);
   const hours = String(Math.floor(seconds / 3600)).padStart(2, '0');
   const minutes = String(Math.floor((seconds % 3600) / 60)).padStart(2, '0');
   const remainingSeconds = String(seconds % 60).padStart(2, '0');
-
-  const formattedTime = `${hours}:${minutes}:${remainingSeconds}`;
-  return formattedTime;
+  return `${hours}:${minutes}:${remainingSeconds}`;
 }
 
 createBot();
